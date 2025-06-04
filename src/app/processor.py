ce44bac199685b6cc7cbabff179a03262a67901b
Mark Quinn <mobious_99@yahoo.com>
2025-06-04 13:51:19 -0400

    chore(docs): fix docstrings and formatting

--- src/app/processor.py
+++ src/app/processor.py
@@ -180,6 +180,8 @@ def analyze_sentiment(
     :param backend: Literal["finbert":
     :param text: type text: str :
     :param backend: type backend: Literal["finbert" :
+    :param text: str:
+    :param backend: Literal["finbert":
     :param text: str: 
     :param backend: Literal["finbert": 
     :param "vader": 
